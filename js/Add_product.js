edf24e0037b611b607fa8a8a28c2b23b3a3da0c2
MertCanGuergoel <38778834+MertCanGuergoel@users.noreply.github.com>
2018-06-20 13:52:27 +0200

    adding som Product
    Script for entering values into tables

---
+++ js/Add_product.js
@@ -0,0 +1,13 @@
+var con = dbconnect();
+
+var sql = "INSERT INTO Product (shelf, name, exdate) Values ?";
+var shelf = document.getElementById('product_shelf');
+var name = document.getElementById('product_name');
+var exdate = document.getElementById('datepicker');
+
+var values =[shelf, name, exdate];
+
+con.query(sql, [Values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
