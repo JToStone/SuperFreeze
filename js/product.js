90ee30703efd4e41ce509021898b87c6036200a7
Marvin <marvin.lanz@besonet.ch>
2018-06-20 14:16:23 +0200

    Fixed product.js

--- js/product.js
+++ js/product.js
@@ -1,13 +1,15 @@
-var con = dbconnect();
+function add_product(){
+  var con = dbconnect();
 
-var sql = "INSERT INTO Product (shelf, name, exdate) Values ?";
-var shelf = document.getElementById('product_shelf');
-var name = document.getElementById('product_name');
-var exdate = document.getElementById('datepicker');
+  var sql = "INSERT INTO Product (shelf, name, exdate) Values ?";
+  var shelf = document.getElementById('product_shelf');
+  var name = document.getElementById('product_name');
+  var exdate = document.getElementById('datepicker');
 
-var values =[shelf, name, exdate];
+  var values =[shelf, name, exdate];
 
-con.query(sql, [Values], function (err, result) {
+  con.query(sql, [Values], function (err, result) {
     if (err) throw err;
     console.log("Number of records inserted: " + result.affectedRows);
   });
+};
