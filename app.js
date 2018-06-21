9befe7849a5a13e9e879d64f1f3a33b01a6f2f60
Aregver <arno@Arnos-MacBook-Pro.local>
2018-06-21 10:31:31 +0200

    Update App.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require(`express`)
 
 var app = express();
+app.use('/css',express.static( 'views/css'));
 
 app.get(`/login`,function(req,res){
   res.render(`login.ejs`);
 })
+
 .get(`/`, function(req,res){
   res.redirect(`/login`);
 })
-.listen(3000);
+.listen(8080);
+console.log('8080 is the magic port');
