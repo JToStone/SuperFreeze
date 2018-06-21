a1e5aac67c2de1f498bc9560ebfd2d2d314f8c3e
Marvin <marvin.lanz@besonet.ch>
2018-06-21 10:45:13 +0200

    Added all Templates to app.js

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ var express = require(`express`)
 
 var app = express();
 app.use('/css',express.static( 'views/css'));
+app.use(`/js`,express.static(`js`));
 
 app.get(`/login`,function(req,res){
   res.render(`login.ejs`);
 })
-
 .get(`/`, function(req,res){
   res.redirect(`/login`);
+})
+.get(`/index`, function(req,res){
+  res.render(`index.ejs`);
+})
+.get(`/addFreezer`, function(req,res){
+  res.render(`freezer.ejs`);
+})
+.get(`/config`, function(req,res){
+  res.render(`configuration.ejs`);
+})
+.get(`/product`, function(req,res){
+  res.render(`product.ejs`);
 })
 .listen(8080);
 console.log('8080 is the magic port');
