32b86009b9bd5291faec820d7a2bbffe6f35c67e
JToStone <jouni@Air-Jonas.local>
2018-06-21 13:13:57 +0200

    ready for sql

---
+++ js/menu-generation.js
@@ -0,0 +1,50 @@
+/*var mysql = require('mysql');
+
+var con = dbconnect();
+*/
+var freezerSQL = "SELECT * FROM Freezer WHERE customer_id = 1";
+
+// counting results
+var freezerCount;
+var shelfCount;
+// resultsets
+var freezerResult;
+var shelfResult;
+
+var freezerName;
+var shelfName;
+
+var html;
+
+
+for (i = 0; i < freezerCount; i++) {
+    // TODO freezerName = "1";
+    html += "<li class='treeview'>";
+    html += "<a href='#'><span> " + freezerName + "</span>";
+    html += "<span class='pull-right-container'>";
+    html += "<i class='fa fa-angle-left pull-right'>";
+    html += "</i></span></a>";
+
+    html += "<ul class='treeview-menu'>";
+    html += "<li class='treeview'>";
+    html += "<a href='#'>Configuration<span class='pull-right-container'>";
+    html += "<i class='fa fa-angle-left pull-right'></i></span></a>";
+    html += "<ul class='treeview-menu'>";
+
+    var shelfSQL = "SELECT * FROM shelf WHERE Freezer_id = " + freezerResult[i].id;
+/*
+    con.query(shelfSQL,
+        function (result) {
+            shelfCount = result.length;
+            shelfResult = result;
+        };
+    );
+*/
+    for (i = 0; i < shelfCount; i++) {
+        // TODO shelfName = "1";
+        html += "<li><a href='#'> " + shelfName + "</a></li>";
+    }
+
+    html += "<li><a href='#'><i class='fa fa-plus'></i> Create Shelf</a></li></ul></li>";
+    html += "<li><a href='#'> Products</a></li></ul></li>";
+}
