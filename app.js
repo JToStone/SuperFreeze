5f41b5620408d856902cebf02ed005218aaf4a38
Marvin <marvin.lanz@besonet.ch>
2018-06-21 11:16:51 +0200

    Mysql Db connection

--- app.js
+++ app.js
@@ -4,13 +4,39 @@ var app = express();
 app.use('/css',express.static( 'views/css'));
 app.use(`/js`,express.static(`js`));
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "10.10.10.102",
+  user: "superfreezer",
+  password: "asdf",
+  database: "superfreezer"
+});
+
 app.get(`/login`,function(req,res){
+
+
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = `SELECT count(customer.id) AS count FROM customer WHERE password=${username} AND username=${password}``;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
+
+
   res.render(`login.ejs`);
+
 })
 .get(`/`, function(req,res){
   res.redirect(`/login`);
 })
 .get(`/index`, function(req,res){
+
+
   res.render(`index.ejs`);
 })
 .get(`/addFreezer`, function(req,res){
